e347633d81dc63d8c82477a2cf107ef6f7b31930
Dmitry Khodyrev <dooommm@mail.ru>
2022-08-19 17:10:04 +0800

    update modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,21 @@
-import React, { Fragment } from 'react';
-import HeaderCartButton from './HeaderCartButton';
-import classes from './Header.module.css';
-import mealsImage from '../../components/assets/meals.jpg';
+import { Fragment } from 'react';
 
-export default function Header(props) {
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
-        <img src={mealsImage} alt='meals' />
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
-}
+};
+
+export default Header;
